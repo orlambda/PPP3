d44d070ed2c5cd3494a1000d6cd31e53a477fce9
orlambda <115938017+orlambda@users.noreply.github.com>
2024-05-02 12:14:39 +0100

    first commit

---
+++ 05 Writing a program/05.03.01 calculator first attempt/main.cpp
@@ -0,0 +1,43 @@
+#include "/Users/orlandoshamlou/Documents/Coding/CPP_CLion/PPP/PPP_support/PPPheaders.h"
+
+int main() {
+    // prompt
+    cout << "Enter an expression (we support '+', '-', '*', '/', and '%'), terminating with '=': ";
+    // get input
+    // calculate
+    double lval;
+    cin >> lval;
+    while(true)
+    {
+        char op = '0';
+        cin >> op;
+        if (op == '=')
+        {
+            break;
+        }
+        double rval;
+        cin >> rval;
+        switch(op)
+        {
+            case '+':
+                lval += rval;
+                break;
+            case '-':
+                lval -= rval;
+                break;
+            case '*':
+                lval *= rval;
+                break;
+            case '/':
+                lval /= rval;
+                break;
+            case '%':
+                lval = fmod(lval, rval);
+                break;
+            default:
+                error("Unrecognised operator");
+        }
+    }
+    // output
+    cout << "Result: " << lval << endl;
+}
