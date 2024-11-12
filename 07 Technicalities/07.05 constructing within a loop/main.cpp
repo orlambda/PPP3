37a733a307182b616783f38455422551627970dd
orlambda <115938017+orlambda@users.noreply.github.com>
2024-11-12 16:42:50 +0000

    Loop over function and add static int

--- 07 Technicalities/07.05 constructing within a loop/main.cpp
+++ 07 Technicalities/07.05 constructing within a loop/main.cpp
@@ -1,27 +1,42 @@
 #include "../../PPP_support/PPPheaders.h"
 
+void do_stuff();
+
 int main() {
-    int i = 0;
-    while (i < 5)
+    for (int i = 0; i < 2; ++i) {
+        do_stuff();
+    }
+}
 
+void do_stuff() {
     {
-        // This gets constructed 5 times
-        vector<int> v;
-        v.push_back(1);
-        // Size is always 1
-        cout << "Vector1 size: " << v.size() << endl;
+        int i = 0;
+        while (i < 5)
 
-        // This gets constructed once
-        static vector<int> v2;
-        v2.push_back(i);
-        cout << "Vector2 size: " << v2.size() << endl;
-        cout << "Vector2 last int: " << v2[v2.size()-1] << endl;
+        {
+            // This gets constructed 5 times
+            vector<int> v;
+            v.push_back(1);
+            // Size is always 1
+            cout << "Vector1 size: " << v.size() << endl;
 
-        int x;
-        // Uninitialized on the first iteration
-        cout << "x is: " << x << endl;
-        x = i;
+            // This gets constructed once
+            static vector<int> v2;
+            v2.push_back(i);
+            cout << "Vector2 (static) size: " << v2.size() << endl;
+            cout << "Vector2 (static) last int: " << v2[v2.size()-1] << endl;
 
-        ++i;
+            int x;
+            // Uninitialized on the first iteration
+            cout << "x: " << x << endl;
+            x = i;
+
+            static int y;
+            // Uninitialized on the first iteration
+            cout << "y (static): " << y << endl;
+            y = i;
+
+            ++i;
+        }
     }
 }
