f0895f21bc2304ed8b839aee7e484d6682649911
orlambda <115938017+orlambda@users.noreply.github.com>
2025-05-23 19:03:47 +0100

    Add integer input in dec, hex, and oct

--- 09 iostreams/09 09.10.01 integer dec hex output/main.cpp
+++ 09 iostreams/09 09.10.01 integer dec hex output/main.cpp
@@ -11,7 +11,22 @@ int main()
 {
     int year = 2012;
     int age = 40;
+
     cout << "\t\t" << "hex" << "\t\t" << "oct" << "\t\t" << "dec" << "\t\t" << "bin" << endl;
     cout << "year:\t" << hex << year << "\t\t" << oct << year << "\t" << dec << year << "\t" << format("{:b}", year) << endl;
     cout << "year:\t" << hex << age << "\t\t" << oct << age << "\t\t" << dec << age << "\t\t" << format("{:b}", age) << endl;
+
+    int age1{};
+    int age2{};
+    int age3{};
+
+    // No error checking!
+    cout << endl << "Input age in dec:" << endl;
+    cin >> dec >> age1;
+    cout << endl << "Input age in hex:" << endl;
+    cin >> hex >> age2;
+    cout << endl << "Input age in oct:" << endl;
+    cin >> oct >> age3;
+    cout << "Ages:" << "\t" << "dec" << "\t" << "hex" << "\t" "oct" << endl;
+    cout << "\t\t" << age1 << "\t" << age2 << "\t" << age3 << "\t" << endl;
 }
