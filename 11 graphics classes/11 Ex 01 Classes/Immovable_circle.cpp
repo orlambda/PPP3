7b3c324b7a1e0b0a676403fa3f013b155ffe7aa4
orlambda <115938017+orlambda@users.noreply.github.com>
2025-06-04 01:56:33 +0100

    Add notes on improving Smiley and Immovable_circle

--- 11 graphics classes/11 Ex 01 Classes/Immovable_circle.cpp
+++ 11 graphics classes/11 Ex 01 Classes/Immovable_circle.cpp
@@ -7,8 +7,10 @@
 // avoiding 'unused parameter' error
 // unused attribute is compiler-specific
 // (void)(b) works for all compilers
+// can also leave parameter unnamed (int, int)
 
 void Immovable_circle::move(__attribute__((unused)) int a, int b) {
     (void)(b);
+    // return;
     throw runtime_error("Cannot move immovable circle");
 }
--- 11 graphics classes/11 Ex 01 Classes/Smiley.h
+++ 11 graphics classes/11 Ex 01 Classes/Smiley.h
@@ -20,15 +20,18 @@ public:
     void draw_specifics(Painter& painter) const override;
     void move(int dx, int dy) override;
 private:
+    // TODO: delete variable
     int r = radius();
     // Facial features - values relative to face radius
+    // TODO: hide from user
+    // name relative/ratio
     static constexpr double eye_radius = 0.2;
     static constexpr double horizontal_eye_offset = 0.3;
     static constexpr double vertical_eye_offset = 0.3;
     static constexpr double mouth_half_width = 0.4;
     static constexpr double mouth_height = 0.1;
     static constexpr double mouth_vertical_offset = 0.3;
-    Circle left_eye {center() + Point{static_cast<int>(-r * horizontal_eye_offset), static_cast<int>(-r*vertical_eye_offset)}, static_cast<int>(r * eye_radius)};
+    Circle left_eye {center() + Point{static_cast<int>(-radius() * horizontal_eye_offset), static_cast<int>(-r*vertical_eye_offset)}, static_cast<int>(r * eye_radius)};
     Circle right_eye {center() + Point{static_cast<int>(r * horizontal_eye_offset), static_cast<int>(-r*vertical_eye_offset)}, static_cast<int>(r * eye_radius)};
     Arc mouth {center() + Point{0,static_cast<int>(r * mouth_vertical_offset)}, static_cast<int>(r * mouth_half_width), static_cast<int>(r * mouth_height), -10, -160};
     // Arc mouth {center(), 50, 50, 0, 180};
