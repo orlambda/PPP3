8e852f223969fa42a6fdd08d249b4b8f5ad94cbe
orlambda <115938017+orlambda@users.noreply.github.com>
2025-07-17 23:10:37 +0100

    Test using strlen() on nullptr on this system

---
+++ 16 arrays pointers references/strlen nullptr/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// Test using strln() on nullptr on this system
+// Following §16.4.3
+
+int main() {
+    // char* p = nullptr;
+    // size_t x = strlen(p);
+    strlen(nullptr);
+}
