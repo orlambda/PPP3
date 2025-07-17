e00de6efdcc54bbd7ed893465010d969c39b4f5d
orlambda <115938017+orlambda@users.noreply.github.com>
2025-07-17 22:07:07 +0100

    Test sizeof() on std::array

--- 15 Vector/sizeof/main.cpp
+++ 15 Vector/sizeof/main.cpp
@@ -13,12 +13,19 @@ int get_size_of(int p[])
     return sizeof(p);
 }
 
+int get_size_of(std::array<int, 5> a)
+{
+    return sizeof(a);
+}
+
 int main()
 {
     // On-stack array
     int a2[5];
     // Free-store-allocated
     int* a3 = new int[5];
+
+    std::array<int, 5> std_arr{};
 
     cout << "Sizes using sizeof()" << endl;
     cout << "Size of int: " << sizeof(int) << endl;
@@ -35,6 +42,9 @@ int main()
     cout << "Size of on-stack array of 5 ints (other scope): " << get_size_of(a2) << endl;
     cout << "Size of dynamic array of 5 ints: " << sizeof(*a3) << " !!" << endl;
     cout << "Number of elements in array (should be 5): " << sizeof(a1)/sizeof(a1[0]) << endl;
+    cout << "Size of std::array of 5 ints: " << sizeof(std_arr) << endl;
+    cout << "Size of std::array of 5 ints (other scope): " << get_size_of(std_arr) << endl;
+    cout << "Number of elements in std::array (should be 5): " << sizeof(std_arr)/sizeof(std_arr[0]) << endl;
 
     return 0;
 }
