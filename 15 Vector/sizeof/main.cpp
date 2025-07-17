25dacebb67f4bdb8f34bbd8863b4b94becb74051
orlambda <115938017+orlambda@users.noreply.github.com>
2025-07-17 21:53:12 +0100

    Test sizeof() on array passed to function

--- 15 Vector/sizeof/main.cpp
+++ 15 Vector/sizeof/main.cpp
@@ -8,6 +8,11 @@ using std::cin, std::cout, std::endl, std::list, std::vector;
 // Static array
 int a1[5];
 
+int get_size_of(int p[])
+{
+    return sizeof(p);
+}
+
 int main()
 {
     // On-stack array
@@ -22,11 +27,13 @@ int main()
     cout << "Size of char*: " << sizeof(char*) << endl;
     cout << "Size of long: " << sizeof(long) << endl;
     cout << "Size of double: " << sizeof(double) << endl;
-    cout << "'Size' of vector of ints: " << sizeof(vector<int>(10)) << endl; // Not checking the size of the container
-    cout << "'Size' of list of vectors of ints: " << sizeof(list<vector<int>>(4)) << endl; // Not checking the size of the container
+    cout << "Size of vector of ints: " << sizeof(vector<int>(10)) << endl;
+    cout << "Size of list of vectors of ints: " << sizeof(list<vector<int>>(4)) << endl;
     cout << "Size of static array of 5 ints: " << sizeof(a1) << endl;
+    cout << "Size of static array of 5 ints (other scope): " << get_size_of(a1) << endl;
     cout << "Size of on-stack array of 5 ints: " << sizeof(a2) << endl;
-    cout << "'Size' of dynamic array of 5 ints: " << sizeof(*a3) << " !!" << endl;
+    cout << "Size of on-stack array of 5 ints (other scope): " << get_size_of(a2) << endl;
+    cout << "Size of dynamic array of 5 ints: " << sizeof(*a3) << " !!" << endl;
     cout << "Number of elements in array (should be 5): " << sizeof(a1)/sizeof(a1[0]) << endl;
 
     return 0;
