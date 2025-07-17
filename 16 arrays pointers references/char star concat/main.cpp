d6e037b36d77bc32d53c770802443d83c325b1a3
orlambda <115938017+orlambda@users.noreply.github.com>
2025-07-17 21:20:58 +0100

    Add project to test code from §16.3

---
+++ 16 arrays pointers references/char star concat/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// Tests code from §16.3
+
+// Concatenate c-style strings with '@' delimiter
+char * concat(char* name, char* address)
+{
+    int name_size = strlen(name);
+    int final_size = name_size + strlen(address) + 2; // +2 for '@' and terminating 0
+    char* result = (char*) malloc(final_size);
+    strcpy (result, name);
+    result[name_size+1] = '@';
+    strcpy(result+name_size+2, address);
+    return result;
+}
+
+int main()
+{
+    std::cout << concat("Tav", "Faerun") << std::endl;
+    return 0;
+}
