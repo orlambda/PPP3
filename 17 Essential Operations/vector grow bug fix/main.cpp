63f3c15cea34846c5a813007b37151b2a6a3cbba
orlambda <115938017+orlambda@users.noreply.github.com>
2025-07-23 01:35:08 +0100

    Fix bug: grow() now compiles and works correctly

--- 17 Essential Operations/vector grow bug fix/main.cpp
+++ 17 Essential Operations/vector grow bug fix/main.cpp
@@ -1,24 +1,30 @@
+// Fixes a bug in grow() from PPP3 §17.7
+
+// now increment n in for loop
+// initialise i in for loop
+// copy vector to double in correct scope
+
 #include <iostream>
 
 using std::cin, std::cout, std::endl, std::vector;
 
-
 void grow(vector<int>& v)
 {
     int n = 0;
-    for (double d; cin >> d; )
+    for (double d; cin >> d; ++n)
     {
         if (n == v.size())
         {
             vector<int> v2(v.size()+1);
-            for (int i; i < v.size(); ++i)
+            for (int i=0; i < v.size(); ++i)
             {
                 v2[i] = v[i];
             }
             v = v2;
         }
+        v[n] = d;
     }
-    v[n] = d;
+
 }
 
 
